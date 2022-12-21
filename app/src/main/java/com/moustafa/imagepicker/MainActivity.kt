27ff89f9cabdd2ba2e444230f52aef0f2d968f2b
“Moustafa <moustafaelsaghier@gmail.com>
2022-12-22 01:19:35 +0200

    implement the logic for selecting both type single/multiple image from gallery

--- app/src/main/java/com/moustafa/imagepicker/MainActivity.kt
+++ app/src/main/java/com/moustafa/imagepicker/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.moustafa.imagepicker
 
 import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.moustafa.imagepicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val MAX_ITEMS = 5 // max number of allowed items to be selected
+
+    private val singleImagePicker by lazy {
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            uri?.let {
+                val selectedImages = StringBuilder()
+                selectedImages.append(uri.toString())
+                binding.selectedImagesUri.text = selectedImages
+            }
+        }
+    }
+
+    private val multipleImagePicker by lazy {
+        registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(MAX_ITEMS)) { selectedUris ->
+            // Callback is invoked after the user selects media items or closes the
+            if (selectedUris.isNotEmpty()) {
+                val selectedImages = StringBuilder()
+                for (uri in selectedUris) {
+                    selectedImages.append(uri.toString())
+                }
+                binding.selectedImagesUri.text = selectedImages
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +42,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setButtonClicks()
+    }
 
-        binding.fab.setOnClickListener { view ->
-
+    private fun setButtonClicks() {
+        binding.singleImagePick.setOnClickListener {
+            singleImagePicker.launch(
+                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+            )
         }
+
+        /**
+         * you can select only 5 images because of value for MAX_ITEMS
+         * */
+        binding.multiImagePick.setOnClickListener {
+            multipleImagePicker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
     }
 
 }
